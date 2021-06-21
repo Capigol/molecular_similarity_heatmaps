2f0ac056a5e60e1ae832f2b02ae29c0106a87187
Capigol <86059300+Capigol@users.noreply.github.com>
2021-06-21 19:25:03 -0300

    Update Similarity_heatmaps_interactive.py

--- Similarity_heatmaps_interactive.py
+++ Similarity_heatmaps_interactive.py
@@ -125,7 +125,7 @@ def heatmap(df_ok):
     if type_plot == True:
         # color = "YlGnBu"
         fig = go.Figure(go.Heatmap(z=df_ok,x0=1,dx=1, y0=1,dy=1, hoverongaps = False,showscale=True, colorscale=plotly_color,zmax=1,zmin=0))
-        fig.update_layout(margin = dict(t=20,r=20,b=20,l=20),
+        fig.update_layout(margin = dict(t=60,r=20,b=20,l=20),
         width = 800, height = 800,
         autosize = False )
         st.plotly_chart(fig)
