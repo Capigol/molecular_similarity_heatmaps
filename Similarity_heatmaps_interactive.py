c2bed6609700e10cb46bb7b8ca3b94a74eaed403
Capigol <86059300+Capigol@users.noreply.github.com>
2021-09-07 12:46:00 -0300

    Update Similarity_heatmaps_interactive.py

--- Similarity_heatmaps_interactive.py
+++ Similarity_heatmaps_interactive.py
@@ -36,7 +36,7 @@ st.image(image)
 
 st.markdown("![Twitter Follow](https://img.shields.io/twitter/follow/LIDeB_UNLP?style=social)")
 st.subheader(":pushpin:" "About Us")
-st.markdown("We are a team interested to develop new tools cheminformatics for using in areas of computer-assisted drug design and machine learning in drug discovery. We belong to Laboratory of Bioactive Research and Development (LIDeB) of the National University of La Plata, Argentina. Our research group is focused on rational designing of new drug candidates and drug repurposing for epilepsy and neglected tropical diseases such as Chagas disease, leishmaniasis, malaria.Another important goal of our group is the development and caracterization of nanocarriers. The work developed by our group has resulted in publications in international indexed journals, abstracts, congress and awards in national and international of Medicinal and Computational Chemistry.")
+st.markdown("We are a team interested to develop new tools cheminformatics for using in areas of computer-assisted drug design and machine learning in drug discovery. We belong to Laboratory of Bioactive Research and Development (LIDeB) of the National University of La Plata, Argentina. Our research group is focused on rational designing of new drug candidates and drug repurposing for epilepsy and neglected tropical diseases such as Chagas disease, leishmaniasis, malaria. Another important goal of our group is the development and caracterization of nanocarriers. The work developed by our group has resulted in publications in international indexed journals, abstracts, congress and awards in national and international of Medicinal and Computational Chemistry.")
 st.markdown(":computer:""**Web Site** " "<https://lideb.biol.unlp.edu.ar>")
 
 
@@ -74,7 +74,12 @@ if type_plot == True:
 else:
     sns_color = st.sidebar.selectbox("Select the heatmap color", ("rocket", "mako", "flare","crest","magma","viridis"),5)
 
-
+st.sidebar.title(":speech_balloon: Contact Us")
+st.sidebar.info(
+"""
+If you are looking to contact us, please
+[:e-mail:](mailto:lideb@biol.unlp.edu.ar) or [🇹](https://twitter.com/LIDeB_UNLP)
+""")
 
 #---------------------------------#
 # Main panel
